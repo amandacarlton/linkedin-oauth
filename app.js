0b7ff20e4a5f0a1fd73b9ff6df170aafc872c129
Amanda Carlton <acarlton27@gmail.com>
2015-07-09 14:13:12 -0600

    link

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
 var session = require('cookie-session');
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+var authRoutes = require('./routes/auth');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +29,35 @@ app.use(session({
   keys:
   [process.env.SESSION_KEY1,
   process.env.SESSION_KEY2] }));
-  
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LinkedInStrategy({
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/linkedin/callback",
+    scope: ['r_emailaddress', 'r_basicprofile'],
+    state: true
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, {id: profile.id, displayName: profile.displayName, token: accessToken});
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
 app.use('/', routes);
+app.use('/', authRoutes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
