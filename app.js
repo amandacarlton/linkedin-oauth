31e498d4c87e45c2cf18f9b2cc8bc9e6fda9b9b0
Amanda Carlton <acarlton27@gmail.com>
2015-07-09 13:42:01 -0600

    env

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+var session = require('cookie-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,12 +16,17 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  keys:
+  [process.env.SESSION_KEY1,
+  process.env.SESSION_KEY2] }));
+  
 app.use('/', routes);
 app.use('/users', users);
 
